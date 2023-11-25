e44ea412f4e7f9b7f736f269a993795b5bf87579
Unaro <vladislav.burado@gmail.com>
2023-11-25 12:52:45 +0600

    Добавьте файлы проекта.

---
+++ ArrayLibrary/CustomArray.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace MyArray.Library
+{
+
+    public class CustomArray : IEnumerable<int>
+    {
+        private int[] _array;
+        public int this[int index]
+        {
+            get => _array[index];
+            set => _array[index] = value;
+        }
+
+        public CustomArray(int capacity) {
+            if (capacity <= 0) throw new ArgumentException();
+            _array = new int[capacity];
+        }
+
+        public int Length => _array.Length;
+
+        public void SetRandomValues()
+        {
+            var random = new Random();
+            foreach (var item in _array)
+            {        
+                _array[item] = random.Next();
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _array.GetEnumerator();
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            return ((IEnumerable<int>)_array).GetEnumerator();
+        }
+    }
+}
